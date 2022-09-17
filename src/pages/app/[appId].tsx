600fa7cae1507d80e9986016d5b5e1ac21a9041b
Josef Vacek <vacekj@outlook.com>
2022-09-17 19:38:49 +0200

    fix: comment out navigation arrows

--- src/pages/app/[appId].tsx
+++ src/pages/app/[appId].tsx
@@ -18,16 +18,28 @@ function App() {
       {/* Actionbar */}
       <div tw="sticky inset-0 bottom-auto bg-black/50 py-2.5 px-2 select-none">
         <div tw="flex justify-center items-center space-x-4">
-          <button tw="opacity-50 cursor-pointer transition-all hover:opacity-100">
+          <button
+            onClick={() => {
+              window.history.back()
+            }}
+            tw="opacity-50 cursor-pointer transition-all hover:opacity-100"
+          >
             <ArrowLeftIcon tw="h-5 w-5" />
           </button>
-          <button tw="opacity-50 cursor-pointer transition-all hover:opacity-100">
-            <ArrowRightIcon tw="h-5 w-5" />
-          </button>
+          {/*<button onClick={() => {*/}
+          {/*  window.history.forward();*/}
+          {/*}} tw="opacity-50 cursor-pointer transition-all hover:opacity-100">*/}
+          {/*  <ArrowRightIcon tw="h-5 w-5" />*/}
+          {/*</button>*/}
           <div tw="bg-white/10 rounded-lg py-0.5 px-3 text-white/50 text-center w-[18rem] text-ellipsis overflow-hidden">
-            https://heroicons.com/
+            {window.location.toString()}
           </div>
-          <button tw="opacity-50 cursor-pointer transition-all hover:opacity-100">
+          <button
+            onClick={() => {
+              router.reload()
+            }}
+            tw="opacity-50 cursor-pointer transition-all hover:opacity-100"
+          >
             <ArrowPathIcon tw="h-5 w-5" />{' '}
           </button>
         </div>
