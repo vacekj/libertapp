905debbfd7c0d58abf975a3b4889f6b8550b874c
Josef Vacek <vacekj@outlook.com>
2022-09-17 16:16:29 +0200

    feat(apps): add uniswap

---
+++ public/apps/uniswap/static/js/69.5b474a6c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@uniswap/interface"]=this["webpackJsonp@uniswap/interface"]||[]).push([[69],{1562:function(s,e,n){"use strict";n.r(e),n.d(e,"default",(function(){return t}));var t={messages:{}}}}]);
+//# sourceMappingURL=69.5b474a6c.chunk.js.map
